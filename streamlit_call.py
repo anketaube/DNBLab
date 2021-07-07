fb02723e14323013aa5974311e08e9fc55481369
anketaube <46967596+anketaube@users.noreply.github.com>
2021-07-07 18:31:36 +0200

    Create streamlit_call.py

---
+++ streamlit_call.py
@@ -0,0 +1,5 @@
+from subprocess import Popen
+
+def load_jupyter_server_extension(nbapp):
+    """serve the streamlit app"""
+    Popen(["streamlit", "hello", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
